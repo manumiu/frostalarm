68413834ac081d945d379e7ffc5149ea43e07ca3
manumiu <manupopp@gmail.com>
2016-08-03 20:59:36 +0200

    03.08.

--- platforms/browser/www/js/weather.js
+++ platforms/browser/www/js/weather.js
@@ -1,16 +1,15 @@
 /** Created by manu on 16.06.2016. */
-// TODO: Nutzereingabe "intelligent" machen (Autocomplete, "Did you mean...?")
 
 // the function which handles the input field logic
 function printLocation() {
-    //var location = document.getElementById('location').value;
-
-    // TODO: Zuordnung Stadt zu City ID
-    // Liste mit City IDs ist unter res/citylist abgelegt
 
     //Abfrage der Daten, die von meinem Webserver ausgegeben werden
     var xhr = new XMLHttpRequest();
     var locationInput = document.getElementById('location').value;
+
+    //Save location
+    localStorage.setItem("location", locationInput);
+    console.log(localStorage.getItem("location"));
 
     //URL and attached user input
     var url = 'http://querbeetberlin.de:8080/'+locationInput;
@@ -19,10 +18,10 @@ function printLocation() {
     xhr.open("GET", url, true);
     xhr.setRequestHeader('Content-Type', 'text/plain');
     xhr.send();
-
-    //Beobachtung: Request wird alle paar Sekunden neu abgeschickt. Zählt das dann jedes Mal als ein eigener Aufruf
-    // der API?
 }
+
+//TODO: #14: Fallunterscheidung, je nachdem ob Wert in localStorage gespeichert ist oder nicht.
+
 
 //diese Funktion gibt die Temperatur für die kommenden 24h aus.
 function reqListener () {
@@ -31,7 +30,7 @@ function reqListener () {
 
     //read text of the xmlhttpRequest
     var xhrResponse = this.responseText;
-
+    console.log(xhrResponse);
     //transform text to JSON object
     var xhrResponseJSN = JSON.parse(xhrResponse);
 
@@ -41,37 +40,35 @@ function reqListener () {
     //call function that retrieves all values for the key temp
     var temperature = getValues(xhrResponseJSN,key);
 
-    //select first 24 temperature values (forecast provides data in 1 hour-steps)
+    //select first 24 temperature values (forecast.io provides data in 1 hour-steps)
     var forecast24 = temperature.slice(0,24);
     console.log(forecast24);
 
     var lowestTemp = Math.min.apply(Math, forecast24);
     console.log(lowestTemp);
 
+    //find paragraph with ID "WeatherResult" to print out the temperature data in it
+    //var owmResult = document.getElementById('weatherResultTemp');
+
+    // print out next 24 temperature values in "weatherResult"
+    //owmResult.textContent = "Erwartetes Temperatur-Minimum der nächsten 24 Stunden: "+lowestTempString+"°C ";
+
     //get element to print out statement about min temperature
     var minResult = document.getElementById('weatherResultText');
 
-    //print out statement depending on temperature minimum in forecast
+    //print out statement depending on temperature minimum in forecast:
 
-    //if temp below or equal 5 degree celcius:
+    //if temp below or equal 5 degree celcius
     if (lowestTemp <= 5){
       minResult.textContent = "Achtung! Heute Nacht sind Temperaturen von "+lowestTemp+" Grad oder weniger zu erwarten."
 
-    //if temperature above 5 degree celcius:
+    //if temperature above 5 degree celcius
     } else {
       minResult.textContent = "Alles fein. Heute Nacht wird es voraussichtlich nicht kälter als "+lowestTemp+" Grad."
     }
 
     var lowestTempString = JSON.stringify(lowestTemp);
 
-    //TODO: prüfen, ob ein wert unterhalb einer Temperatur x enthalten ist.
-
-
-    //find paragraph with ID "WeatherResult" to print out the temperature data in it
-    var owmResult = document.getElementById('weatherResultTemp');
-
-    // print out next 24 temperature values in "weatherResult"
-    owmResult.textContent = "Erwartetes Temperatur-Minimum der nächsten 24 Stunden: "+lowestTempString+"°C ";
 }
 
 //generic function to retrieve keys in JSON data
@@ -103,11 +100,9 @@ function printLocationHelper () {
 
 
 //use eventlistener for click on LocationButton
-var LocationButton = document.getElementById('LocationButton');
-LocationButton.addEventListener('click', printLocationHelper, false);
+var locationButton = document.getElementById('locationButton');
+locationButton.addEventListener('click', printLocationHelper, false);
 
-
-// was machen die folgenden zwei Zeilen?
-// use an eventlistener for the event
-var subButton = document.getElementById('subButton');
-subButton.addEventListener('click', printLocation, false);
+// use an eventlistener for click on forecastButton
+var forecastButton = document.getElementById('forecastButton');
+forecastButton.addEventListener('click', printLocation, false);
